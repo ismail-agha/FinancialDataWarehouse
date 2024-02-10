d226e51757c397e7fbc33e2c2e54e303f1e85be4
ismail-agha <ismailaga118@gmail.com>
2024-02-10 23:42:39 +0530

    commit

--- data_loads/data_load_equity_list.py
+++ data_loads/data_load_equity_list.py
@@ -1,3 +1,11 @@
+import os
+import sys
+
+# Add parent directory to the Python path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
 import pandas as pd
 from db.database_and_models import TABLE_MODEL_EQUITY_LIST, session
 import json
