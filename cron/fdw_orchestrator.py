323b107b1ca45a92e65ff21e5da906cef0422c33
ismail-agha <ismailaga118@gmail.com>
2024-05-05 13:08:37 +0530

    commit

--- cron/fdw_orchestrator.py
+++ cron/fdw_orchestrator.py
@@ -16,11 +16,11 @@ virtual_env_python = os.path.join(parent_dir, "venv", "bin", "python3")
 def execute_script(script_path):
     try:
         # Path to the script you want to run
-        script_path_a = os.path.join(parent_dir, f"{script_path}")
+        script_path = os.path.join(parent_dir, f"{script_path}")
 
-        print(f'parent_dir = {parent_dir}')
-        print(f'virtual_env_python = {virtual_env_python}')
-        print(f'script_path_a = {script_path_a}')
+        #print(f'parent_dir = {parent_dir}')
+        #print(f'virtual_env_python = {virtual_env_python}')
+        #print(f'script_path_a = {script_path_a}')
 
         #subprocess.run(["/usr/bin/python3", script_path], check=True) /home/ec2-user/FinancialDataWarehouse/venv/bin
         subprocess.run([virtual_env_python, script_path], check=True)
