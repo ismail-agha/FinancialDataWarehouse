b19870ae1626cb203102cd8812e269d8f225647a
ismail-agha <ismailaga118@gmail.com>
2024-05-05 17:09:35 +0530

    commit

--- cron/fdw_orchestrator.py
+++ cron/fdw_orchestrator.py
@@ -51,7 +51,7 @@ def main():
 def stop_instance(instance_id):
     try:
         # Stop the EC2 instance
-        ec2_client.stop_instances(InstanceIds=[instance_id])
+        #ec2_client.stop_instances(InstanceIds=[instance_id])
         print(f"Instance {instance_id} stopped successfully.")
         custom_logging(logger, 'INFO', f"Instance {instance_id} stopped successfully.")
     except Exception as e:
