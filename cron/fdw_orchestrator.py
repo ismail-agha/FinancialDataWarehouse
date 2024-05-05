93c0fc88b8f00ddd621ba30b479825dcc883d1e2
ismail-agha <ismailaga118@gmail.com>
2024-05-05 17:09:17 +0530

    commit

--- cron/fdw_orchestrator.py
+++ cron/fdw_orchestrator.py
@@ -2,6 +2,13 @@ import subprocess
 import boto3
 import sys
 import os
+from generic.custom_logging_script import setup_logger, custom_logging
+
+# Define the script name here
+script_name = os.path.basename(__file__).split('.')[0]
+
+# Initialize the logger with the script name
+logger = setup_logger(script_name)
 
 # Initialize the Boto3 EC2 client
 ec2_client = boto3.client('ec2', region_name='ap-south-1')
@@ -24,8 +31,11 @@ def execute_script(script_path):
 
         #subprocess.run(["/usr/bin/python3", script_path], check=True) /home/ec2-user/FinancialDataWarehouse/venv/bin
         subprocess.run([virtual_env_python, script_path], check=True)
+
+        custom_logging(logger, 'INFO', f'Completed {script_path}')
     except subprocess.CalledProcessError as e:
         print(f"Error executing {script_path}: {e}")
+        custom_logging(logger, 'ERROR', f"Error executing {script_path}: {e}")
         exit(1)
 
 
@@ -36,12 +46,6 @@ def main():
     # Execute script2.py upon successful completion of script1.py
     #execute_script("/path/to/script2.py")
 
-    # Execute script3.py upon successful completion of script2.py
-    #execute_script("/path/to/script3.py")
-
-    # Execute script4.py upon successful completion of script3.py
-    #execute_script("/path/to/script4.py")
-
 
 # Stop EC2 Instance
 def stop_instance(instance_id):
@@ -49,8 +53,11 @@ def stop_instance(instance_id):
         # Stop the EC2 instance
         ec2_client.stop_instances(InstanceIds=[instance_id])
         print(f"Instance {instance_id} stopped successfully.")
+        custom_logging(logger, 'INFO', f"Instance {instance_id} stopped successfully.")
     except Exception as e:
         print(f"Error stopping instance {instance_id}: {e}")
+        custom_logging(logger, 'ERROR', f"Error stopping instance {instance_id}: {e}")
+
 
 if __name__ == "__main__":
     main()
---
+++ generic/custom_logging_script.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import logging
+from datetime import datetime
+
+def setup_logger(script_name):
+    # Add parent directory to the Python path
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    parent_dir = os.path.dirname(current_dir)
+    sys.path.append(parent_dir)
+
+    # Create a timestamp string
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    # Configure the logging settings
+    log_filename = os.path.join(parent_dir, f"logs/{script_name}_{timestamp}.log")
+
+    logging.getLogger("requests").setLevel(logging.WARNING)
+    logging.getLogger("urllib3").setLevel(logging.WARNING)
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        filename=log_filename,
+        filemode='w'
+    )
+
+    # Create a logger object
+    logger = logging.getLogger(__name__)
+
+    return logger
+
+def custom_logging(logger, type='INFO', msg=''):
+    if type == 'INFO':
+        logger.info(msg)
+    elif type == 'ERROR':
+        logger.error(msg)
