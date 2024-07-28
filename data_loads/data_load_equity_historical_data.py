14b6dad33f95e49ecae4ad88a75d654f3f05f3be
Ismail <ismailaga118@gmail.com>
2024-07-28 17:02:29 +0530

    commit

--- data_loads/data_load_equity_historical_data.py
+++ data_loads/data_load_equity_historical_data.py
@@ -44,7 +44,7 @@ def main():
     #print(result_df)
 
     interval = 'day'
-    from_date = '2024-02-05'
+    from_date = '2000-01-01'
     to_date = datetime.now().strftime("%Y-%m-%d")
 
     total_rows = len(result_df)
