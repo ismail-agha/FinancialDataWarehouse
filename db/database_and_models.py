3c765d0fb595f19050520451ab99845636e1c311
ismail <ismailaga118@gmail.com>
2025-06-27 23:08:33 +0530

    commit

--- db/database_and_models.py
+++ db/database_and_models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, select, insert, text
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, select, insert, text, PrimaryKeyConstraint
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.ext.declarative import declarative_base
